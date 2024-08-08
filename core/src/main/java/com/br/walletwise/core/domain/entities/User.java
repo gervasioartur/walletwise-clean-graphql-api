1916d0317eb9b60ef38e6d918a66fd0c2d9e054e
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-08 04:10:02 -0300

    feat: ensure User throws DomainException when trying to build User with lastname empty or null

--- core/src/main/java/com/br/walletwise/core/domain/entities/User.java
+++ core/src/main/java/com/br/walletwise/core/domain/entities/User.java
@@ -95,6 +95,8 @@ public class User extends  AbstractEntity{
         List<IValidator> validators = new ArrayList<>();
         validators.addAll(ValidationBuilder.of("Firstname",this.firstname).required().build());
         validators.addAll(ValidationBuilder.of("Lastname",this.lastname).required().build());
+        validators.addAll(ValidationBuilder.of("Username",this.username).required().build());
+
         return validators;
     }
 }
