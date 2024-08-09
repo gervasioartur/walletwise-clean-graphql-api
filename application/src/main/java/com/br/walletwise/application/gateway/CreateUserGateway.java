8205237c80e3025784364c8c91fdba0e24bb0e4e
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-09 04:13:59 -0300

    refactor: change folder names to use singular names

--- application/src/main/java/com/br/walletwise/application/gateway/CreateUserGateway.java
+++ application/src/main/java/com/br/walletwise/application/gateway/CreateUserGateway.java
@@ -1,6 +1,6 @@
 package com.br.walletwise.application.gateway;
 
-import com.br.walletwise.core.domain.entities.User;
+import com.br.walletwise.core.domain.entity.User;
 
 public interface CreateUserGateway {
     User create(User user);
--- usecase/src/main/java/com/br/walletwise/usecase/CreateUser.java
+++ usecase/src/main/java/com/br/walletwise/usecase/CreateUser.java
@@ -1,6 +1,6 @@
 package com.br.walletwise.usecase;
 
-import com.br.walletwise.core.domain.entities.User;
+import com.br.walletwise.core.domain.entity.User;
 
 public interface CreateUser {
     void create(User user);
