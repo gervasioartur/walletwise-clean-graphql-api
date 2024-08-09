9928190598da6ef3d7cca442dddb86af0b0b9a52
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-09 04:22:28 -0300

    test: ensure RequiredFieldValidator return error message is field value zero

--- core/src/test/java/com/br/walletwise/core/validation/RequiredFieldValidatorTests.java
+++ core/src/test/java/com/br/walletwise/core/validation/RequiredFieldValidatorTests.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RequiredFieldValidatorTests {
     private RequiredFieldValidator validator;
@@ -13,6 +14,19 @@ public class RequiredFieldValidatorTests {
     @NullAndEmptySource
     @DisplayName("Should return error message if the field value is empty")
     void shouldReturnErrorMessageIfFieldValueIsEmpty(String fieldValue) {
+        String fieName = "any_field_name";
+        this.validator = new RequiredFieldValidator(fieName,fieldValue );
+        String error  = this.validator.validate();
+
+        Assertions.assertThat(error).isNotEmpty();
+        Assertions.assertThat(error).isEqualTo("any_field_name is required.");
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0.0})
+    @DisplayName("Should return error message if the field value is zero")
+    void shouldReturnErrorMessageIfFieldValueIsZero(double fieldValue) {
         String fieName = "any_field_name";
         this.validator = new RequiredFieldValidator(fieName,fieldValue );
         String error  = this.validator.validate();
