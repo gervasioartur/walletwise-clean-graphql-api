f9afb30537db30e9d0f8ae0c8a5f3f1d9d36806d
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-09 04:15:38 -0300

    feat: add start up app

---
+++ infra/src/main/java/com/br/walletwise/infra/WalletWiseApplication.java
@@ -0,0 +1,11 @@
+package com.br.walletwise.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletWiseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WalletWiseApplication.class);
+    }
+}
