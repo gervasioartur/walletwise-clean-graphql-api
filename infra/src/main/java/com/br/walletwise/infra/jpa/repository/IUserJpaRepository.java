7b6bfd71f9869feb72b0619558fe58c975d50858
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-09 06:14:11 -0300

    fix: make sure IUserJpaRepository return only actved users

--- infra/src/main/java/com/br/walletwise/infra/jpa/repository/IUserJpaRepository.java
+++ infra/src/main/java/com/br/walletwise/infra/jpa/repository/IUserJpaRepository.java
@@ -2,12 +2,16 @@ package com.br.walletwise.infra.jpa.repository;
 
 import com.br.walletwise.infra.jpa.entity.UserJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface IUserJpaRepository extends JpaRepository<UserJpaEntity, UUID> {
-    Optional<UserJpaEntity> findByUsername(String username);
+    @Query("SELECT user FROM UserJpaEntity  user WHERE  user.username = :username AND user.active = true")
+    Optional<UserJpaEntity> findByUsername(@Param("username") String username);
 
-    Optional<UserJpaEntity> findByEmail(String email);
+    @Query("SELECT user FROM UserJpaEntity user WHERE user.email = :email  AND user.active = true")
+    Optional<UserJpaEntity> findByEmail(@Param("email") String email);
 }
