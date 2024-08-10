734b52dd6dd863a18deca20d4566e3d0eff36fdc
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-10 14:04:11 -0300

    test: ensure AuthenticateUser throws UnauthorizedException if password is wrong

--- application/src/test/java/com/br/walletwise/application/usecaseimpl/AuthenticateUserImplTests.java
+++ application/src/test/java/com/br/walletwise/application/usecaseimpl/AuthenticateUserImplTests.java
@@ -1,6 +1,9 @@
 package com.br.walletwise.application.usecaseimpl;
 
+import com.br.walletwise.application.gateway.AuthenticateUserGateway;
+import com.br.walletwise.application.mocks.MocksFactory;
 import com.br.walletwise.application.usecasesimpl.AuthenticateUserImpl;
+import com.br.walletwise.core.domain.entity.User;
 import com.br.walletwise.core.exception.UnauthorizedException;
 import com.br.walletwise.usecase.AuthenticateUser;
 import com.br.walletwise.usecase.FindByEmail;
@@ -11,39 +14,64 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class AuthenticateUserImplTests {
     private AuthenticateUser authenticateUser;
 
     private FindByUsername findByUsername;
     private FindByEmail findByEmail;
+    private AuthenticateUserGateway authenticateUserGateway;
 
     @BeforeEach
     void setUp() {
         this.findByUsername =  mock(FindByUsername.class);
         this.findByEmail =  mock(FindByEmail.class);
-        this.authenticateUser = new AuthenticateUserImpl(findByUsername,findByEmail);
+        this.authenticateUserGateway = mock(AuthenticateUserGateway.class);
+        this.authenticateUser = new AuthenticateUserImpl(findByUsername,findByEmail, authenticateUserGateway);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"any_username","any_username@test.com"})
     @DisplayName("Should throw UnauthorizedException if user does not exist by username or email")
     void shouldThrowUnauthorizedExceptionIfUserDoesNotExistByUsernameOrEmail(String usernameOrEmail) {
-        String password = "password";
+        String password = "any_password";
 
-        if(usernameOrEmail.contains("@"))
-            when(this.findByEmail.find(usernameOrEmail)).thenReturn(Optional.empty());
-        else
-            when(this.findByUsername.find(usernameOrEmail)).thenReturn(Optional.empty());
+        when(this.findByEmail.find(usernameOrEmail)).thenReturn(Optional.empty());
+        when(this.findByUsername.find(usernameOrEmail)).thenReturn(Optional.empty());
 
         Throwable exception = catchThrowable(() -> this.authenticateUser.authenticate(usernameOrEmail, password));
 
         assertThat(exception).isInstanceOf(UnauthorizedException.class);
         assertThat(exception.getMessage()).isEqualTo("Bad credentials.");
+        if(usernameOrEmail.contains("@"))
+            verify(this.findByEmail, times(1)).find(usernameOrEmail);
+        else
+            verify(this.findByUsername, times(1)).find(usernameOrEmail);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"any_username","any_username@test.com"})
+    @DisplayName("Should throw UnauthorizedException if password is wrong")
+    void shouldThrowUnauthorizedExceptionIfPasswordIsWrong(String usernameOrEmail) {
+        String password = "any_password";
+        User user = MocksFactory.userFactory();
+
+        when(this.findByEmail.find(usernameOrEmail)).thenReturn(Optional.of(user));
+        when(this.findByUsername.find(usernameOrEmail)).thenReturn(Optional.of(user));
+        when(this.authenticateUserGateway.authenticate(usernameOrEmail, password)).thenReturn(null);
+
+        Throwable exception = catchThrowable(() -> this.authenticateUser.authenticate(usernameOrEmail, password));
+
+        assertThat(exception).isInstanceOf(UnauthorizedException.class);
+        assertThat(exception.getMessage()).isEqualTo("Bad credentials.");
+        if(usernameOrEmail.contains("@"))
+            verify(this.findByEmail, times(1)).find(usernameOrEmail);
+        else
+            verify(this.findByUsername, times(1)).find(usernameOrEmail);
     }
 }
