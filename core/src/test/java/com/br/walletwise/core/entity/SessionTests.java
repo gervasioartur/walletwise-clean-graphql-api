31852889523e0bc7d61451063a31a0ddb3bdd4e6
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-10 09:13:08 -0300

    test: ensure Session throw DomainException if user id is nul on build session with all arguments

---
+++ core/src/test/java/com/br/walletwise/core/entity/SessionTests.java
@@ -0,0 +1,31 @@
+package com.br.walletwise.core.entity;
+
+import com.br.walletwise.core.domain.entity.Session;
+import com.br.walletwise.core.exception.DomainException;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
+public class SessionTests {
+    private Faker faker = new Faker();
+
+    @Test
+    @DisplayName("Should throw DomainException if user id is null on build session with session id")
+    void shouldThrowDomainExceptionIfUserIdIsNullOnBuildSessionWithSessionId() {
+        UUID id = UUID.randomUUID();
+        UUID userId = null;
+        String token = UUID.randomUUID().toString();
+        LocalDateTime expirationDate = LocalDateTime.now();
+        LocalDateTime creationDate = LocalDateTime.now().plusMinutes(15);
+
+        Throwable exception = catchThrowable(() -> new  Session(id ,userId, token, expirationDate, creationDate));
+
+        assertThat(exception).isInstanceOf(DomainException.class);
+        assertThat(exception.getMessage()).isEqualTo("User is required.");
+    }
+}
