28bfcaa3a6060cb9f2adcfb87170ed3fae4364c3
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-11 04:16:24 -0300

    chore: add Session table creation to migrations

--- infra/src/main/resources/migrations/V1__create_user_table.sql
+++ infra/src/main/resources/migrations/V1__create_user_table.sql
@@ -3,12 +3,23 @@ CREATE schema IF NOT EXISTS security;
 CREATE TABLE IF NOT EXISTS security.t_users
 (
     id         UUID         NOT NULL PRIMARY KEY,
-    firstname VARCHAR(100) NOT NULL,
-    lastname  VARCHAR(100) NOT NULL,
+    firstname  VARCHAR(100) NOT NULL,
+    lastname   VARCHAR(100) NOT NULL,
     username   VARCHAR(100) NOT NULL UNIQUE,
     email      VARCHAR(100) NOT NULL UNIQUE,
     password   VARCHAR(64)  NOT NULL,
-    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    is_active  BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS security.t_sessions
+(
+    id            UUID         NOT NULL PRIMARY KEY,
+    token         VARCHAR(300) NOT NULL,
+    fk_user_id    UUID NOT NULL REFERENCES security.t_users,
+    creation_date TIMESTAMP  NOT NULL,
+    is_active     BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at    TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
