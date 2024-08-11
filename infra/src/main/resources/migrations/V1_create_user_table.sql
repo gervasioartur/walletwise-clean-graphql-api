e260019fe527a602602451f16e981ccf96b082b8
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-11 09:31:53 -0300

    refactor: rename migration to from V1__create_user_table.sql to V1_create_user_table.sql

--- infra/src/main/resources/migrations/V1_create_user_table.sql
+++ infra/src/main/resources/migrations/V1_create_user_table.sql
@@ -12,14 +12,3 @@ CREATE TABLE IF NOT EXISTS security.t_users
     created_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE IF NOT EXISTS security.t_sessions
-(
-    id            UUID         NOT NULL PRIMARY KEY,
-    token         VARCHAR(300) NOT NULL,
-    fk_user_id    UUID NOT NULL REFERENCES security.t_users,
-    creation_date TIMESTAMP  NOT NULL,
-    is_active     BOOLEAN NOT NULL DEFAULT TRUE,
-    created_at    TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at    TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
