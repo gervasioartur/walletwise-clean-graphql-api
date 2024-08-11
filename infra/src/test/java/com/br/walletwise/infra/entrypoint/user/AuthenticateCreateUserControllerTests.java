26cd5010fafdeeba1c9799b50bc6ac352fa8fe85
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-11 09:40:10 -0300

    test: ensure AuthenticateUser throws InternalServerError if usecase throws unexpected exception

---
+++ infra/src/test/java/com/br/walletwise/infra/entrypoint/user/AuthenticateCreateUserControllerTests.java
@@ -0,0 +1,78 @@
+package com.br.walletwise.infra.entrypoint.user;
+
+import com.br.walletwise.infra.entrypoint.dto.AuthenticateUserRequest;
+import com.br.walletwise.infra.mappers.UserMapper;
+import com.br.walletwise.infra.mocks.MocksFactory;
+import com.br.walletwise.usecase.AuthenticateUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class AuthenticateCreateUserControllerTests {
+    private final String URL = "/users/authenticate";
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockBean
+    private MockMvc mvc;
+
+    @MockBean
+    private AuthenticateUser usecase;
+    @MockBean
+    private UserMapper mapper;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"any_username", "anyuser@email.com"})
+    @DisplayName("Should return InternalServerError if an unexpected exception occurs")
+    public void shouldReturnInternalServerErrorIfAnUnexpectedExceptionOccurs(String usernameOrEmail) throws Exception {
+        AuthenticateUserRequest requestParams = new AuthenticateUserRequest
+                (usernameOrEmail, MocksFactory.faker.internet().password());
+
+        String json = new ObjectMapper().writeValueAsString(requestParams);
+
+        doThrow(HttpServerErrorException.InternalServerError.class).when(this.usecase)
+                .authenticate(requestParams.usernameOrEmail(), requestParams.password());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(this.URL)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc
+                .perform(request)
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("body",
+                        Matchers.is("An unexpected error occurred. Please try again later.")));
+
+        verify(usecase, times(1))
+                .authenticate(requestParams.usernameOrEmail(), requestParams.password());
+    }
+}
