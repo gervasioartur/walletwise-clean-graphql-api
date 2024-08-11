f172dc31d1a264f3fea044a019501198e4c184fa
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-11 02:55:04 -0300

    feat: add authentication bean configuration

--- infra/src/main/java/com/br/walletwise/infra/config/ApplicationConfig.java
+++ infra/src/main/java/com/br/walletwise/infra/config/ApplicationConfig.java
@@ -5,6 +5,11 @@ import com.br.walletwise.application.usecasesimpl.EncodePasswordImpl;
 import com.br.walletwise.usecase.EncodePassword;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,5 +24,18 @@ public class ApplicationConfig {
     @Bean
     public EncodePassword encodePassword(EncodePasswordGateway encodePasswordGateway) {
         return new EncodePasswordImpl(encodePasswordGateway);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
     }
 }
