8856e06cd5c27a41da706d21172b4be7c1173aff
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-11 03:03:10 -0300

    refactor: remove unused code

--- infra/src/main/resources/migrations/V1__create_user_table.sql
+++ infra/src/main/resources/migrations/V1__create_user_table.sql
@@ -12,18 +12,3 @@ CREATE TABLE IF NOT EXISTS security.t_users
     created_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
--- CREATE OR REPLACE FUNCTION security.fn_update_timestamp_column()
---     RETURNS TRIGGER AS $$
---     BEGIN
---         NEW.updated_at = NOW();
---         RETURN NEW;
---     END;
---     $$
--- LANGUAGE plpgsql;
---
--- CREATE TRIGGER security.tr_set_timestamp
---     BEFORE UPDATE
---     ON security.t_users
---     FOR EACH ROW
---     EXECUTE FUNCTION security.fn_update_timestamp_column();
