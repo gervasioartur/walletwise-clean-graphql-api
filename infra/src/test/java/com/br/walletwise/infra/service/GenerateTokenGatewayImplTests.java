a94ee2e133e0f4a70b0f0b097418c5347adf6a8b
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-11 03:01:55 -0300

    test: ensure GenerateTokenGatewayImpl generates token

---
+++ infra/src/test/java/com/br/walletwise/infra/service/GenerateTokenGatewayImplTests.java
@@ -0,0 +1,41 @@
+package com.br.walletwise.infra.service;
+
+import com.br.walletwise.application.gateway.GenerateTokenGateway;
+import com.br.walletwise.infra.helpers.CreateToken;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class GenerateTokenGatewayImplTests {
+    @Autowired
+    private GenerateTokenGateway generateTokenGateway;
+    @MockBean
+    private CreateToken createToken;
+
+
+    @Test
+    @DisplayName("Should return generated token")
+    public void shouldReturnGeneratedToken() {
+        String username =  "any_username";
+        String token = UUID.randomUUID().toString();
+        Map<String, Object> claims = new HashMap<>();
+
+        when(this.createToken.create(claims, username)).thenReturn(token);
+
+        String result = this.generateTokenGateway.generate(username);
+
+        assertThat(result).isEqualTo(token);
+        verify(this.createToken).create(claims, username);
+    }
+}
