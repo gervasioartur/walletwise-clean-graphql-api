896ccc020b67bfee8d3d5da21528d02818e0769e
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-12 04:53:39 -0300

    test: ensure Expense throws Domain exception if user info is null

--- core/src/test/java/com/br/walletwise/core/entity/ExpenseTests.java
+++ core/src/test/java/com/br/walletwise/core/entity/ExpenseTests.java
@@ -32,4 +32,19 @@ class ExpenseTests {
         assertThat(exception.getMessage()).isEqualTo("User info is required.");
     }
 
+    @Test
+    @DisplayName("Should throw DomainException if user info is null on build with no id")
+    void shouldThrowDomainExceptionIfUserIdIsNullOnBuildWithNoId() {
+        Throwable exception = catchThrowable(() -> new Expense(
+                null,
+                faker.lorem().word(),
+                CategoryEnum.RENT.getValue(),
+                ExpenseTypeEnum.FIXED.getValue(),
+                new BigDecimal(faker.number().randomNumber()),
+                true));
+
+        assertThat(exception).isInstanceOf(DomainException.class);
+        assertThat(exception.getMessage()).isEqualTo("User info is required.");
+    }
+
 }
