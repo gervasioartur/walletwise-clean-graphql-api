d8a00efc256c41784de01caba7ae92d1125f99c0
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-14 14:13:37 -0300

    test: ensure FixedExpense throws DomainException if user info is null

--- application/src/test/java/com/br/walletwise/application/mocks/MocksFactory.java
+++ application/src/test/java/com/br/walletwise/application/mocks/MocksFactory.java
@@ -2,6 +2,7 @@ package com.br.walletwise.application.mocks;
 
 import com.br.walletwise.core.domain.entity.Session;
 import com.br.walletwise.core.domain.entity.User;
+
 import com.github.javafaker.Faker;
 
 import java.util.UUID;
@@ -23,6 +24,17 @@ public class MocksFactory {
     public static User userFactory() {
         return new User(
                 UUID.randomUUID(),
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.name().username(),
+                faker.internet().emailAddress(),
+                "Password!1234H",
+                true);
+    }
+
+    public static User userFactory(UUID userId) {
+        return new User(
+                userId,
                 faker.name().firstName(),
                 faker.name().lastName(),
                 faker.name().username(),
