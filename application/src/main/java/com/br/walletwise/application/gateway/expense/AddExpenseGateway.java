ee33098a6eb45a46a826e991e5dfd88afcc943fe
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-14 14:07:19 -0300

    refactor: change classes to use just FixedExpense instead of Expense

--- application/src/main/java/com/br/walletwise/application/gateway/expense/AddExpenseGateway.java
+++
@@ -1,7 +0,0 @@
-package com.br.walletwise.application.gateway.expense;
-
-import com.br.walletwise.core.domain.entity.Expense;
-
-public interface AddExpenseGateway {
-    Expense addExpense(Expense expense);
-}
--- usecase/src/main/java/com/br/walletwise/usecase/expense/AddExpense.java
+++
@@ -1,7 +0,0 @@
-package com.br.walletwise.usecase.expense;
-
-import com.br.walletwise.core.domain.entity.Expense;
-
-public interface AddExpense {
-    Expense add(Expense expense);
-}
