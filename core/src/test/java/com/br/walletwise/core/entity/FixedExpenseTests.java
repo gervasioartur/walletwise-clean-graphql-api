b3748cc8a2c35c1e7de0e0340f74c7967d287706
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-14 06:16:47 -0300

    test: ensure FixedExpense throws Domain exception if start date is null on build FixedExpense with no id

--- core/src/test/java/com/br/walletwise/core/entity/FixedExpenseTests.java
+++ core/src/test/java/com/br/walletwise/core/entity/FixedExpenseTests.java
@@ -58,4 +58,25 @@ class FixedExpenseTests {
         assertThat(exception).isInstanceOf(DomainException.class);
         assertThat(exception.getMessage()).isEqualTo("Start date is required.");
     }
+
+    @Test
+    @DisplayName("Should throw DomainException if start date is null on update start date")
+    void shouldThrowDomainExceptionIfStartDateIsNullOnUpdateStartDate() {
+        FixedExpense fixedExpense =  new FixedExpense(
+                20,
+                new Date(),
+                Date.from(LocalDateTime.now().plusDays(20).atZone(ZoneId.systemDefault()).toInstant()),
+                01,
+                UUID.randomUUID(),
+                faker.lorem().paragraph(),
+                CategoryEnum.SCHOOL.getValue(),
+                ExpenseTypeEnum.FIXED.getValue(),
+                new BigDecimal(200),
+                true);
+
+        Throwable exception = Assertions.catchThrowable(() -> fixedExpense.setStartDate(null));
+
+        assertThat(exception).isInstanceOf(DomainException.class);
+        assertThat(exception.getMessage()).isEqualTo("Start date is required.");
+    }
 }
