83d726757a32510a789576c125a41dc7e9369fa0
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-15 09:09:03 -0300

    feat: ensure GetLoggedUser  returns logged user

--- application/src/main/java/com/br/walletwise/application/usecasesimpl/user/GetLoggedUserImpl.java
+++ application/src/main/java/com/br/walletwise/application/usecasesimpl/user/GetLoggedUserImpl.java
@@ -1,17 +1,18 @@
 package com.br.walletwise.application.usecasesimpl.user;
 
+import com.br.walletwise.application.gateway.user.GetLoggedUserGateway;
 import com.br.walletwise.core.domain.entity.User;
 import com.br.walletwise.usecase.user.GetLoggedUser;
 
 public class GetLoggedUserImpl implements GetLoggedUser {
-    private final GetLoggedUser getLoggedUser;
+    private final GetLoggedUserGateway getLoggedUserGateway;
 
-    public GetLoggedUserImpl(GetLoggedUser getLoggedUser) {
-        this.getLoggedUser = getLoggedUser;
+    public GetLoggedUserImpl(GetLoggedUserGateway getLoggedUserGateway) {
+        this.getLoggedUserGateway = getLoggedUserGateway;
     }
 
     @Override
     public User get() {
-        return this.getLoggedUser.get();
+        return this.getLoggedUserGateway.get();
     }
 }
