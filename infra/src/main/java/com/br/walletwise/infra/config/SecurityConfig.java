01903df9312c60a445bc132bc3ca203a4b016361
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-15 15:26:15 -0300

    migrations: change V3__create_fixed_expense_table to use big serial instead of bit int

--- infra/src/main/java/com/br/walletwise/infra/config/SecurityConfig.java
+++ infra/src/main/java/com/br/walletwise/infra/config/SecurityConfig.java
@@ -33,7 +33,6 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        ;
         return http.build();
 
     }
