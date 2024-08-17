8c2459b78198f9cde48aa57857f3c4fd49632031
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-17 00:02:52 -0300

    test: ensure AddToRedisCacheGatewayImpl add JsonValue to redis cache

--- infra/src/test/java/com/br/walletwise/infra/service/cache/AddToRedisCacheGatewayImplTests.java
+++ infra/src/test/java/com/br/walletwise/infra/service/cache/AddToRedisCacheGatewayImplTests.java
@@ -37,10 +37,26 @@ class AddToRedisCacheGatewayImplTests {
     void shouldThrowUnexpectedExceptionOnAddToRedisCache() throws JsonProcessingException {
         String key = "key";
         String value = "value";
-        String jsonValue = "any_json_value";
         when(this.mapper.writeValueAsString(value)).thenThrow(JsonProcessingException.class);
         Throwable exception = catchThrowable(() -> this.addToCacheGateway.add(key,value));
         assertThat(exception).isInstanceOf(UnexpectedException.class);
         verify(this.mapper, times(1)).writeValueAsString(value);
+    }
+
+    @Test
+    @DisplayName("Should add key and value to cache")
+    void shouldAddKeyAndValueToCache() throws JsonProcessingException {
+        String key = "key";
+        String value = "value";
+        String jsonValue = "any_json_value";
+        String cachedValue = "cached_value";
+
+        when(this.mapper.writeValueAsString(value)).thenReturn(jsonValue);
+        when(this.jedis.set(key,jsonValue)).thenReturn(cachedValue);
+
+        this.addToCacheGateway.add(key,value);
+
+        verify(this.mapper, times(1)).writeValueAsString(value);
+        verify(this.jedis).set(key,jsonValue);
     }
 }
