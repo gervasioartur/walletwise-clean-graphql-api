84fb0db747ed7fa0e7061fbbaf043af0aa5d6f62
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-17 01:59:47 -0300

    test: ensure GetFixedExpense returns optional of FixedExpense

---
+++ application/src/test/java/com/br/walletwise/application/usecaseimpl/expense/GetFixedExpenseImplTests.java
@@ -0,0 +1,43 @@
+package com.br.walletwise.application.usecaseimpl.expense;
+
+import com.br.walletwise.application.gateway.expense.GetFixedExpenseGateway;
+import com.br.walletwise.application.gateway.expense.GetFixedExpensesGateway;
+import com.br.walletwise.application.mocks.MocksFactory;
+import com.br.walletwise.application.usecasesimpl.expense.GetFixedExpenseImpl;
+import com.br.walletwise.core.domain.entity.FixedExpense;
+import com.br.walletwise.core.domain.entity.User;
+import com.br.walletwise.usecase.expense.GetFixedExpense;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+class GetFixedExpenseImplTests {
+    private GetFixedExpense getFixedExpense;
+    private GetFixedExpenseGateway getFixedExpenseGateway;
+
+    @BeforeEach
+    void setUp() {
+        this.getFixedExpenseGateway = mock(GetFixedExpenseGateway.class);
+        this.getFixedExpense = new GetFixedExpenseImpl(getFixedExpenseGateway);
+    }
+
+    @Test
+    @DisplayName("Should return optional of fixed expense ")
+    void shouldReturnEmptyOptionalOfFixedEpense() {
+        User user =  MocksFactory.userFactory();
+        FixedExpense fixedExpense =  MocksFactory.fixedExpenseFactory(user);
+
+        when(this.getFixedExpenseGateway.get(user.getId(),fixedExpense.getId())).thenReturn(Optional.empty());
+
+        Optional<FixedExpense> result = this.getFixedExpense.get(user.getId(),fixedExpense.getId());
+
+        assertThat(result).isEmpty();
+        verify(this.getFixedExpenseGateway, times(1)).get(user.getId(),fixedExpense.getId());
+    }
+}
