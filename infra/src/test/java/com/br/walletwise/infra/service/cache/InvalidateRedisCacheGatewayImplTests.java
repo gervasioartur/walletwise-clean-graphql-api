f6172e6b614c114553b9a700c9fedfc8fb3effc4
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-17 18:46:02 -0300

    test: ensure InvalidateRedisCacheGatewayImpl invalidates cache

--- infra/src/test/java/com/br/walletwise/infra/service/cache/InvalidateRedisCacheGatewayImplTests.java
+++ infra/src/test/java/com/br/walletwise/infra/service/cache/InvalidateRedisCacheGatewayImplTests.java
@@ -1,7 +1,6 @@
 package com.br.walletwise.infra.service.cache;
 
 import com.br.walletwise.application.gateway.cache.InvalidateCacheGateway;
-import com.br.walletwise.usecase.cache.InvalidateCache;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +22,7 @@ class InvalidateRedisCacheGatewayImplTests {
     void invalidateCache() {
         String key = "key";
         when(this.jedis.del(key)).thenReturn(1L);
-        this.jedis.del(key);
+        this.invalidateCacheGateway.delete(key);
         verify(this.jedis, times(1)).del(key);
     }
 }
