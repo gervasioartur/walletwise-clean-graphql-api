904bacc7adef8039199b850f0f56573e84e2d3af
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-17 22:40:26 -0300

    test: ensure GetFixedExpenseGateway returns FixedExpense model

--- infra/src/test/java/com/br/walletwise/infra/service/expenses/GetFixedExpenseGatewayImplTests.java
+++ infra/src/test/java/com/br/walletwise/infra/service/expenses/GetFixedExpenseGatewayImplTests.java
@@ -3,6 +3,7 @@ package com.br.walletwise.infra.service.expenses;
 import com.br.walletwise.application.gateway.expense.GetFixedExpenseGateway;
 import com.br.walletwise.core.domain.entity.FixedExpense;
 import com.br.walletwise.core.domain.entity.User;
+import com.br.walletwise.core.domain.model.FixedExpenseModel;
 import com.br.walletwise.infra.mappers.FixedExpenseMapper;
 import com.br.walletwise.infra.mocks.MocksFactory;
 import com.br.walletwise.infra.persistence.entity.FixedExpenseJpaEntity;
@@ -42,6 +43,24 @@ public class GetFixedExpenseGatewayImplTests {
         assertThat(result).isPresent();
         assertThat(result.get().getId()).isEqualTo(fixedExpense.getId());
         verify(this.mapper, times(1)).mapToDomainObj(entity);
+        verify(this.repository, times(1)).findByIdAndUserId(entity.getId(), entity.getUser().getId());
+    }
+
+    @Test
+    @DisplayName("Should return optional of fixed expense model")
+    public void shouldReturnOptionalOfFixedExpenseModel() {
+        User user = MocksFactory.userFactory();
+        FixedExpenseJpaEntity entity = MocksFactory.fixedExpenseJpaEntityFactory(user);
+        FixedExpenseModel fixedExpense = MocksFactory.fixedExpenseModelFactory(entity);
+
+        when(this.repository.findByIdAndUserId(entity.getId(), entity.getUser().getId())).thenReturn(Optional.of(entity));
+        when(this.mapper.map(entity)).thenReturn(fixedExpense);
+
+        Optional<FixedExpenseModel> result = this.getFixedExpenseGateway.getModel(entity.getId(), entity.getUser().getId());
+
+        assertThat(result).isPresent();
+        assertThat(result.get().getCategory()).isEqualTo(fixedExpense.getCategory());
+        verify(this.mapper, times(1)).map(entity);
         verify(this.repository, times(1)).findByIdAndUserId(entity.getId(), entity.getUser().getId());
     }
 }
