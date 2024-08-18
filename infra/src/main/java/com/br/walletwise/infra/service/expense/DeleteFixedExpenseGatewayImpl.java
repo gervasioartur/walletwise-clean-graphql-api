42036338c313eb5017a6d1e1abbd7f4f3f391896
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-18 00:22:02 -0300

    feat: ensure DeleteFixedExpenseGateway deletes fixed expense

---
+++ infra/src/main/java/com/br/walletwise/infra/service/expense/DeleteFixedExpenseGatewayImpl.java
@@ -0,0 +1,22 @@
+package com.br.walletwise.infra.service.expense;
+
+import com.br.walletwise.application.gateway.expense.DeleteFixedExpenseGateway;
+import com.br.walletwise.core.domain.entity.FixedExpense;
+import com.br.walletwise.infra.mappers.FixedExpenseMapper;
+import com.br.walletwise.infra.persistence.entity.FixedExpenseJpaEntity;
+import com.br.walletwise.infra.persistence.repository.FixedExpenseJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteFixedExpenseGatewayImpl implements DeleteFixedExpenseGateway {
+    private final FixedExpenseJpaRepository repository;
+    private final FixedExpenseMapper mapper;
+
+    @Override
+    public void delete(FixedExpense fixedExpense) {
+        FixedExpenseJpaEntity entity = this.mapper.map(fixedExpense);
+        repository.save(entity);
+    }
+}
