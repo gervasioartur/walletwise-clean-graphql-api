4085594073c28a29bdc622efd4834a82047329bc
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-18 00:20:27 -0300

    feat: ensure DeleteFixedExpense invalidates cache after delete

--- application/src/main/java/com/br/walletwise/application/usecasesimpl/expense/DeleteFixedExpenseImpl.java
+++ application/src/main/java/com/br/walletwise/application/usecasesimpl/expense/DeleteFixedExpenseImpl.java
@@ -3,6 +3,7 @@ package com.br.walletwise.application.usecasesimpl.expense;
 import com.br.walletwise.application.gateway.expense.DeleteFixedExpenseGateway;
 import com.br.walletwise.core.domain.entity.FixedExpense;
 import com.br.walletwise.core.exception.NotFoundException;
+import com.br.walletwise.usecase.cache.InvalidateCache;
 import com.br.walletwise.usecase.expense.DeleteFixedExpense;
 import com.br.walletwise.usecase.expense.GetFixedExpense;
 
@@ -10,12 +11,15 @@ import com.br.walletwise.usecase.expense.GetFixedExpense;
 public class DeleteFixedExpenseImpl implements DeleteFixedExpense {
     private final GetFixedExpense getFixedExpense;
     private final DeleteFixedExpenseGateway deleteFixedExpenseGateway;
+    private final InvalidateCache invalidateCache;
 
     public DeleteFixedExpenseImpl(GetFixedExpense getFixedExpense,
-                                  DeleteFixedExpenseGateway deleteFixedExpenseGateway) {
+                                  DeleteFixedExpenseGateway deleteFixedExpenseGateway,
+                                  InvalidateCache invalidateCache) {
 
         this.getFixedExpense = getFixedExpense;
         this.deleteFixedExpenseGateway = deleteFixedExpenseGateway;
+        this.invalidateCache = invalidateCache;
     }
 
     @Override
@@ -24,5 +28,6 @@ public class DeleteFixedExpenseImpl implements DeleteFixedExpense {
                 .orElseThrow(() -> new NotFoundException("Resource not found."));
         result.setActive(false);
         this.deleteFixedExpenseGateway.delete(result);
+        this.invalidateCache.delete("fixedExpenses:" + result.getUserId());
     }
 }
