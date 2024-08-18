df651c150252a903a8a47c2675547f0a8873e333
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-18 00:20:40 -0300

    test: ensure DeleteFixedExpense invalidates cache after delete

--- application/src/test/java/com/br/walletwise/application/usecaseimpl/expense/DeleteFixedExpenseImplTests.java
+++ application/src/test/java/com/br/walletwise/application/usecaseimpl/expense/DeleteFixedExpenseImplTests.java
@@ -6,6 +6,7 @@ import com.br.walletwise.application.usecasesimpl.expense.DeleteFixedExpenseImpl
 import com.br.walletwise.core.domain.entity.FixedExpense;
 import com.br.walletwise.core.domain.entity.User;
 import com.br.walletwise.core.exception.NotFoundException;
+import com.br.walletwise.usecase.cache.InvalidateCache;
 import com.br.walletwise.usecase.expense.DeleteFixedExpense;
 import com.br.walletwise.usecase.expense.GetFixedExpense;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,12 +24,16 @@ class DeleteFixedExpenseImplTests {
 
     private GetFixedExpense getFixedExpense;
     private DeleteFixedExpenseGateway deleteFixedExpenseGateway;
+    private InvalidateCache invalidateCache;
 
     @BeforeEach
     void setUp() {
         this.getFixedExpense = mock(GetFixedExpense.class);
+        this.invalidateCache = mock(InvalidateCache.class);
         this.deleteFixedExpenseGateway = mock(DeleteFixedExpenseGateway.class);
-        this.deleteFixedExpense = new DeleteFixedExpenseImpl(getFixedExpense, deleteFixedExpenseGateway);
+        this.deleteFixedExpense = new DeleteFixedExpenseImpl(getFixedExpense,
+                deleteFixedExpenseGateway,
+                invalidateCache);
     }
 
     @Test
@@ -56,10 +61,12 @@ class DeleteFixedExpenseImplTests {
         when(this.getFixedExpense.get(expenseCode)).thenReturn(Optional.of(fixedExpense));
         fixedExpense.setActive(false);
         doNothing().when(this.deleteFixedExpenseGateway).delete(fixedExpense);
+        doNothing().when(this.invalidateCache).delete("fixedExpenses:" + user.getId());
 
         this.deleteFixedExpense.delete(expenseCode);
 
         verify(this.getFixedExpense, times(1)).get(expenseCode);
         verify(this.deleteFixedExpenseGateway, times(1)).delete(fixedExpense);
+        verify(this.invalidateCache, times(1)).delete("fixedExpenses:" + user.getId());
     }
 }
